31428feb4ce4175b48998feaa37e757b4532fde7
Luan Donizeti Lucio <luandonizeti2010@hotmail.com>
2012-09-20 19:34:29 -0300

    Crinado visual

---
+++ FilmesAssistidos/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace FilmesAssistidos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewGroup listViewGroup3 = new System.Windows.Forms.ListViewGroup("Filmes", System.Windows.Forms.HorizontalAlignment.Left);
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.labelNomeFilme = new System.Windows.Forms.Label();
+            this.labelDataAssitido = new System.Windows.Forms.Label();
+            this.labelGenero = new System.Windows.Forms.Label();
+            this.labelLocal = new System.Windows.Forms.Label();
+            this.comboBoxGenero = new System.Windows.Forms.ComboBox();
+            this.textBoxLocal = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.buttonPesquisar = new System.Windows.Forms.Button();
+            this.buttonAdicionar = new System.Windows.Forms.Button();
+            this.buttonEditar = new System.Windows.Forms.Button();
+            this.buttonDeletar = new System.Windows.Forms.Button();
+            this.maskedTextBoxData = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(15, 74);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(227, 20);
+            this.textBoxNome.TabIndex = 1;
+            // 
+            // labelNomeFilme
+            // 
+            this.labelNomeFilme.AutoSize = true;
+            this.labelNomeFilme.Location = new System.Drawing.Point(12, 55);
+            this.labelNomeFilme.Name = "labelNomeFilme";
+            this.labelNomeFilme.Size = new System.Drawing.Size(65, 13);
+            this.labelNomeFilme.TabIndex = 5;
+            this.labelNomeFilme.Text = "Nome Filme:";
+            // 
+            // labelDataAssitido
+            // 
+            this.labelDataAssitido.AutoSize = true;
+            this.labelDataAssitido.Location = new System.Drawing.Point(275, 98);
+            this.labelDataAssitido.Name = "labelDataAssitido";
+            this.labelDataAssitido.Size = new System.Drawing.Size(77, 13);
+            this.labelDataAssitido.TabIndex = 6;
+            this.labelDataAssitido.Text = "Data Assistido:";
+            // 
+            // labelGenero
+            // 
+            this.labelGenero.AutoSize = true;
+            this.labelGenero.Location = new System.Drawing.Point(275, 52);
+            this.labelGenero.Name = "labelGenero";
+            this.labelGenero.Size = new System.Drawing.Size(45, 13);
+            this.labelGenero.TabIndex = 7;
+            this.labelGenero.Text = "Gênero:";
+            // 
+            // labelLocal
+            // 
+            this.labelLocal.AutoSize = true;
+            this.labelLocal.Location = new System.Drawing.Point(12, 97);
+            this.labelLocal.Name = "labelLocal";
+            this.labelLocal.Size = new System.Drawing.Size(36, 13);
+            this.labelLocal.TabIndex = 8;
+            this.labelLocal.Text = "Local:";
+            // 
+            // comboBoxGenero
+            // 
+            this.comboBoxGenero.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxGenero.FormattingEnabled = true;
+            this.comboBoxGenero.Items.AddRange(new object[] {
+            "Ação",
+            "Aventura",
+            "Comédia",
+            "Terror",
+            "Suspense",
+            "Documentário",
+            "Infantil",
+            "Romance",
+            "Ficção-científica;"});
+            this.comboBoxGenero.Location = new System.Drawing.Point(278, 73);
+            this.comboBoxGenero.Name = "comboBoxGenero";
+            this.comboBoxGenero.Size = new System.Drawing.Size(130, 21);
+            this.comboBoxGenero.TabIndex = 10;
+            // 
+            // textBoxLocal
+            // 
+            this.textBoxLocal.Location = new System.Drawing.Point(15, 118);
+            this.textBoxLocal.Name = "textBoxLocal";
+            this.textBoxLocal.Size = new System.Drawing.Size(225, 20);
+            this.textBoxLocal.TabIndex = 11;
+            this.textBoxLocal.TextChanged += new System.EventHandler(this.textBoxLocal_TextChanged);
+            // 
+            // listView1
+            // 
+            listViewGroup3.Header = "Filmes";
+            listViewGroup3.Name = "listViewGroupFilmes";
+            this.listView1.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
+            listViewGroup3});
+            this.listView1.Location = new System.Drawing.Point(12, 167);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(490, 189);
+            this.listView1.TabIndex = 12;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // buttonPesquisar
+            // 
+            this.buttonPesquisar.Location = new System.Drawing.Point(210, 12);
+            this.buttonPesquisar.Name = "buttonPesquisar";
+            this.buttonPesquisar.Size = new System.Drawing.Size(75, 37);
+            this.buttonPesquisar.TabIndex = 13;
+            this.buttonPesquisar.Text = "Pesquisar";
+            this.buttonPesquisar.UseVisualStyleBackColor = true;
+            // 
+            // buttonAdicionar
+            // 
+            this.buttonAdicionar.Location = new System.Drawing.Point(15, 12);
+            this.buttonAdicionar.Name = "buttonAdicionar";
+            this.buttonAdicionar.Size = new System.Drawing.Size(75, 37);
+            this.buttonAdicionar.TabIndex = 14;
+            this.buttonAdicionar.Text = "Adicionar";
+            this.buttonAdicionar.UseVisualStyleBackColor = true;
+            this.buttonAdicionar.Click += new System.EventHandler(this.buttonAdicionar_Click);
+            // 
+            // buttonEditar
+            // 
+            this.buttonEditar.Location = new System.Drawing.Point(116, 12);
+            this.buttonEditar.Name = "buttonEditar";
+            this.buttonEditar.Size = new System.Drawing.Size(75, 37);
+            this.buttonEditar.TabIndex = 15;
+            this.buttonEditar.Text = "Editar";
+            this.buttonEditar.UseVisualStyleBackColor = true;
+            // 
+            // buttonDeletar
+            // 
+            this.buttonDeletar.Location = new System.Drawing.Point(320, 12);
+            this.buttonDeletar.Name = "buttonDeletar";
+            this.buttonDeletar.Size = new System.Drawing.Size(75, 37);
+            this.buttonDeletar.TabIndex = 17;
+            this.buttonDeletar.Text = "Deletar";
+            this.buttonDeletar.UseVisualStyleBackColor = true;
+            // 
+            // maskedTextBoxData
+            // 
+            this.maskedTextBoxData.Location = new System.Drawing.Point(278, 118);
+            this.maskedTextBoxData.Mask = "00/00/0000";
+            this.maskedTextBoxData.Name = "maskedTextBoxData";
+            this.maskedTextBoxData.Size = new System.Drawing.Size(88, 20);
+            this.maskedTextBoxData.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 367);
+            this.Controls.Add(this.maskedTextBoxData);
+            this.Controls.Add(this.buttonDeletar);
+            this.Controls.Add(this.buttonEditar);
+            this.Controls.Add(this.buttonAdicionar);
+            this.Controls.Add(this.buttonPesquisar);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBoxLocal);
+            this.Controls.Add(this.comboBoxGenero);
+            this.Controls.Add(this.labelLocal);
+            this.Controls.Add(this.labelGenero);
+            this.Controls.Add(this.labelDataAssitido);
+            this.Controls.Add(this.labelNomeFilme);
+            this.Controls.Add(this.textBoxNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.Label labelNomeFilme;
+        private System.Windows.Forms.Label labelDataAssitido;
+        private System.Windows.Forms.Label labelGenero;
+        private System.Windows.Forms.Label labelLocal;
+        private System.Windows.Forms.ComboBox comboBoxGenero;
+        private System.Windows.Forms.TextBox textBoxLocal;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button buttonPesquisar;
+        private System.Windows.Forms.Button buttonAdicionar;
+        private System.Windows.Forms.Button buttonEditar;
+        private System.Windows.Forms.Button buttonDeletar;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxData;
+
+    }
+}
+
---
+++ FilmesAssistidos/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FilmesAssistidos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdicionar_Click(object sender, EventArgs e)
+        {
+            // Validação dos campos
+
+            if (textBoxNome.Text == "" || textBoxLocal.Text == "" || comboBoxGen.SelectedIndex == 0)
+                MessageBox.Show("Favor preencher os campos", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else 
+            {
+                // Cria o novo item (primeira coluna) campo Nome
+                novoItem = new ListViewItem();
+                novoItem.Text = textBoxNome.Text;
+
+                // Cria o primeiro subItem (segunda coluna) campo Gênero
+                ListViewItem.ListViewSubItem SubitemGenero = new ListViewItem.ListViewSubItem();
+                SubitemGenero.Text = comboBoxGen.SelectedItem.ToString();
+
+                // Cria o segundo subitem (terceira coluna) campo Local
+                ListViewItem.ListViewSubItem SubitemLocal = new ListViewItem.ListViewSubItem();
+                SubitemLocal.Text = textBoxLocal.Text;
+
+                // Cria o terceiro subItem (quarta coluna) campo Data
+                ListViewItem.ListViewSubItem SubitemData = new ListViewItem.ListViewSubItem();
+                string data = dateTimePickerData.Value.Date.ToString();
+                // Remove o horario do componente dateTimePicker, deixando apenas a data
+                data = data.Remove(10);
+                SubitemData.Text = data;
+
+                //Adiciona os subitens ao item 
+                novoItem.SubItems.Add(SubitemGenero);
+                novoItem.SubItems.Add(SubitemLocal);
+                novoItem.SubItems.Add(SubitemData);
+
+                //Adiciona o item ao List View
+                listViewFilmes.Items.Add(novoItem);
+
+
+
+
+         }
+
+        private void textBoxLocal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
